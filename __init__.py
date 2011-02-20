a7a043b6cf1c580bb34ca892c26e8b2b0678f56d
Neil Tallim <flan@uguu.ca>
2011-02-20 10:16:00 -0700

    Added an init-module.

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+(package)
+=========
+Purpose
+-------
+Provides a top-level package for the language implementation.
+
+Meta
+----
+:Authors:
+    Neil Tallim <flan@uguu.ca>
+
+:Version: 1.0.0 : Feb. 20, 2011
+
+Legal
+-----
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a
+letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+"""
+
