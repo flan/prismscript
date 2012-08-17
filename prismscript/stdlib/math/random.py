7d806bc232f1fa9fb72c096c501f8f0bdf042d0f
Neil Tallim <n.tallim@ivrnet.com>
2012-08-17 17:47:12 -0600

    Thought a recursion error was avoided, but nope.

--- prismscript/stdlib/math/random.py
+++ prismscript/stdlib/math/random.py
@@ -18,20 +18,20 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unpo
 To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a
 letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 """
-import random
+import random as _random
 __no_recurse = (
- random,
+ _random,
 )
 
 def randfloat(low=0.0, high=1.0, **kwargs):
-    return random.uniform(low, high)
+    return _random.uniform(low, high)
     
 def randint(high, low=0, **kwargs):
-    return random.randint(low, high)
+    return _random.randint(low, high)
     
 def choose(items, **kwargs):
-    return random.choice(items)
+    return _random.choice(items)
     
 def sample(items, sample_size, **kwargs):
-    return random.sample(items, sample_size)
+    return _random.sample(items, sample_size)
     
